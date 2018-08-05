4299a005a74e823e7c2e40c01e3544095f701637
atulp <atulkp.eee13@nituk.ac.in>
2018-08-05 20:26:17 +0530

    atul|mern.frontend|dataIntegration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080\n' +
+            '/staticdata/getUserCollection')
+            .then(res => {
+                console.log(res);
+                this.setState({contacts: res.data.responseBody});
+            });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">
+                            CONTACTS LIST
+                        </h3>
+                    </div>
+                    <div className="panel-body">
+                        <h4><Link to="/create"><span className="glyphicon glyphicon-plus-sign"
+                                                     aria-hidden="true"></span> Add Contact</Link></h4>
+                        <table className="table table-stripe">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.contacts.map(c =>
+                                <tr>
+                                    <td><Link to={`/show/${c.id}`}>{c.name}</Link></td>
+                                    <td>{c.location}</td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
